18196e88510d8b32f1e1c83f17c94ae2e3e7dfbe
victor <103121630+victor-914@users.noreply.github.com>
2024-04-14 14:05:39 +0100

    legacy

--- app.js
+++ app.js
@@ -2,6 +2,7 @@
 const express = require("express");
 const app = express();
 const port = 5000;
+const axios = require("axios")
 
 // Static Files
 app.use(express.static("public"));
@@ -9,13 +10,61 @@ app.use(express.static("public"));
 // app.use("/js", express.static(__dirname + "public/js"));
 // app.use("/img", express.static(__dirname + "public/img"));
 
+async function fetchData(url) {
+  try {
+    const response = await axios.get(url);
+    return response.data;
+  } catch (error) {
+    console.error("Error fetching data:", error);
+    throw error;
+  }
+}
+
 // Set Views
 app.set("views", "./views");
 app.set("view engine", "ejs");
 
-app.get("", (req, res) => {
-  res.render("index", { text: "This is EJS" });
+app.get("", async (req, res) => {
+  const articles = await fetchData(
+    "https://backend.doxgamingconcept.com/api/articles?populate[images][fields][0]=url&fields[0]=*&pagination[pageSize]=5&pagination[page]=1"
+  );
+  console.log("🚀 ~ app.get ~ articles:", articles)
+
+  res.render("index", {  article: articles });
 });
+
+
+app.get("/contact", (req, res) => {
+  res.render("contact", { text: "This is EJS" });
+});
+
+
+app.get("/partners", (req, res) => {
+  res.render("partners", { text: "This is EJS" });
+});
+
+
+app.get("/post", async (req, res) => {
+
+  console.log(req.query,"query")
+
+  const article = await fetchData(
+    `https://backend.doxgamingconcept.com/api/articles/${parseInt(req.query.id)}?populate[images][fields][0]=url&fields[0]=*&pagination[pageSize]=5&pagination[page]=1`
+  );
+  console.log("🚀 ~ app.get ~ article:", article)
+
+  res.render("post", { article: article });
+});
+
+
+app.get("/blog", async (req, res) => {
+
+  const articles = await fetchData(
+    "https://backend.doxgamingconcept.com/api/articles?populate[images][fields][0]=url&fields[0]=*&pagination[pageSize]=5&pagination[page]=1"
+  );
+  res.render("blog", { article: articles });
+});
+
 
 //  Listen on port 3000
 app.listen(port, () => console.info(`Listening on port ${port}`));
